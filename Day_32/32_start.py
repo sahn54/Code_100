1b1e1ff6694b6fd1f5988bef6db30ea349ca8a2b
Sung Ahn <36002724+sahn54@users.noreply.github.com>
2023-04-26 13:25:33 -0400

    Update 32_start.py

--- Day_32/32_start.py
+++ Day_32/32_start.py
@@ -1,12 +1,12 @@
 import smtplib
 
-# my_email = "sungpower177@gmail.com"
-# password = "yvnmixdwegciqwus"
+# my_email = "@gmail.com"
+# password = ""
 # with smtplib.SMTP("smtp.gmail.com") as connection:
 #     connection.starttls()
 #     connection.login(user=my_email, password=password)
 #     connection.sendmail(from_addr=my_email,
-#                         to_addrs="sungpower424@gmail.com", msg="Subject:Hello\n\n This is the body of my email.")
+#                         to_addrs="@gmail.com", msg="Subject:Hello\n\n This is the body of my email.")
 
 import datetime as dt
 
